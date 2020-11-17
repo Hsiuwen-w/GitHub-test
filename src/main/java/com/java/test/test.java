475d05aad88a2311b8dfede53613eb4c4ee36f69
javakckckc_global <javakckckc_global@javakc.com>
2020-11-17 12:22:41 +0800

    GitHub-test2测试冲突2

--- src/main/java/com/java/test/test.java
+++ src/main/java/com/java/test/test.java
@@ -1,7 +1,10 @@
 package com.java.test;
 
 public class test {
+    //GitHub-test2测试提交
+    private String name="GitHub-test2";
+
     //GitHub-test1测试提交
-    private String name="GitHub-test1";
+    private String name2="GitHub-test1";
 
 }
